9ec2cacd103a068dfb6f271091e44ec8adc63733
josano <josano@hotmail.com>
2016-11-09 17:40:26 -0300

    Retira hardcoded do controller e ajusta a view

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,5 +1,5 @@
 class RecipesController < ApplicationController
 	def index
-		@recipes = ['Strogonoff','Filé de Frango a Milanesa','Salmão ao forno','Bacalhau ao forno']
+		@recipes = Recipe.all
 	end
 end
