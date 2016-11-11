4aa6b81323e31114a144746fb91f0908d0598a61
josano <josano@hotmail.com>
2016-11-11 15:45:07 -0300

    Criando helpers

--- app/helpers/recipes_helper.rb
+++ app/helpers/recipes_helper.rb
@@ -1,2 +1,10 @@
 module RecipesHelper
+
+  def formata_caloria(recipe)
+    if recipe.ligth?
+  	  content_tag(:strong, 'Light até 100 Calorias')
+    else
+      recipe.calories	
+    end	
+  end
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,6 @@
 class Recipe < ApplicationRecord
+  #todo metodo terminado com interrogação retorna um booleano true ou false	
+  def ligth?
+  	calories <= 100
+  end
 end
