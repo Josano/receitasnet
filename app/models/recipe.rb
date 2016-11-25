bb29adbcf7cfbe477cc1b32934f5e3595c25c24e
josano <josano@hotmail.com>
2016-11-25 14:06:18 -0300

    Inserindo validacao nos formularios e model

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,14 @@
 class Recipe < ApplicationRecord
+  #validando os campos que sao obrigatorios
+  validates :name, :stuff, :duration, :prepare_mode, presence:true
+  validates_length_of :stuff, maximum: 200
+  validates_length_of :prepare_mode, maximum: 300
+  validates_numericality_of :calories, greater_than_or_equal_to:0, presence:true
+  validates_format_of :poster, with: /\w+\.(gif|jpg|png)\z/i, message:" Utilizar somente arquivos GIF,JPEG ou PNG."
+  
+  KIND = %w(Carnes Aves Peixes Massas Saladas Doces)
+  validates_inclusion_of :kind, in: KIND
+
   #todo metodo terminado com interrogação retorna um booleano true ou false	
   def ligth?
   	calories <= 100
